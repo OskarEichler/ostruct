b3a4d10da8cb59ab922354ce10b0d850fe6276a9
Oskar Eichler <oskar@songstats.com>
2026-08-28 03:04:15 +0300

    Check OpenStruct equality without dispatching to kind_of?

--- lib/ostruct.rb
+++ lib/ostruct.rb
@@ -424,7 +424,7 @@ class OpenStruct
   #   first_pet == third_pet    # => false
   #
   def ==(other)
-    return false unless other.kind_of?(OpenStruct)
+    return false unless ::OpenStruct === other
     @table == other.table!
   end
 
@@ -434,7 +434,7 @@ class OpenStruct
   # eql?.
   #
   def eql?(other)
-    return false unless other.kind_of?(OpenStruct)
+    return false unless ::OpenStruct === other
     @table.eql?(other.table!)
   end
 
